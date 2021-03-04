ffe7f5d08a4a239360a784c6a8f55f80ceacdaf6
shadykh <eng.shady94@gmail.com>
2021-03-04 10:04:02 +0200

    test3

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@
 // LinkedIn: https://www.linkedin.com/in/walafifi/
 
 'use strict';
+let nameU = prompt('name');
+let age = prompt('age');
+
+
+function printName (name,age){
+    console.log(name,age)
+}
+
+printName(nameU,age);
