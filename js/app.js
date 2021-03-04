e78d5c6756b9429dd993e5340051be3015fd5cb6
Waleed A. Afifi <waleedafifich@gmail.com>
2021-03-04 09:53:06 +0200

    project structur

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Created by: Waleed A. Afifi
+// Email: waleed-afifi@windowslive.com
+// GitHub: https://github.com/waleedafifi90
+// LinkedIn: https://www.linkedin.com/in/walafifi/
+
+'use strict';
